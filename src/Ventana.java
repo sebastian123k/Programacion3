91a65d3ce6fd1be00002a176e26b27ccfdadc31c
sebastian123k <159938783+sebastian123k@users.noreply.github.com>
2024-02-27 05:34:18 +0100

    unidad 2: ejercicio 20

--- src/Ventana.java
+++ src/Ventana.java
@@ -6,274 +6,254 @@ import javax.swing.*;
 
 public class Ventana extends JFrame {
 	
+	
 	JPanel panel = new JPanel();
-	JPanel panel2 = new JPanel();
-	JPanel panel3 = new JPanel();
 	
 	public Ventana()
 	{
-		
-		this.setSize(1000,800);
+		this.setSize(440,380);
 		this.setLayout(null);
-		
-		//this.agregarElementos();
-		//this.agregarRegistro();
-		
-		this.admin();	
+		this.setTitle("Calculadora");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.agregarElementos();
+		this.add(panel);
 		this.setVisible(true);
 	
 	}
 	
-	public void admin(){
-		
-		panel3.setBounds(0,0,1000,800);
-		panel3.setLayout(null);
-		panel3.setBackground(Color.green);
-		
-		JMenuBar barMain = new JMenuBar();
-		JMenu menuFile = new JMenu("Archivo");
-		JMenuItem openItem = new JMenuItem("abrir archivo...");
-		JMenuItem createItem = new JMenuItem("crear archivo...");
-		barMain.add(menuFile);
-		menuFile.add(createItem);
-		menuFile.add(openItem);
-		this.setJMenuBar(barMain);
-		
-		JLabel lblUsuarios = new JLabel("USUARIOS");
-		Font fontIngresar = new Font(Font.MONOSPACED,Font.ITALIC,40);
-		lblUsuarios.setFont(fontIngresar);
-		lblUsuarios.setBounds(350,20,300,50);
-		panel3.add(lblUsuarios);
-		
-		JLabel titleWitged = new JLabel("total de usuarios",0);
-		titleWitged.setBounds(40,100,300,100);
-		titleWitged.setFont(new Font(Font.MONOSPACED,Font.ITALIC,20));
-		titleWitged.setForeground(Color.white);
-		panel3.add(titleWitged);
-		
-		JLabel contendWitged = new JLabel("100",0);
-		contendWitged.setBounds(40,120,300,100);
-		contendWitged.setFont(new Font(Font.MONOSPACED,Font.ITALIC,20));
-		contendWitged.setForeground(Color.white);
-		panel3.add(contendWitged);
-		
-		JLabel witged = new JLabel("");
-		witged.setBounds(40,120,300,100);
-		witged.setOpaque(true);
-		witged.setBackground(Color.black);
-		panel3.add(witged);
-		
-
-		
-		JButton btnDonwload = new JButton("exportar");
-		btnDonwload.setBounds(440,140,100,50);
-		btnDonwload.setBackground(Color.red);
-		panel3.add(btnDonwload);
-		
-		JButton btnAdd= new JButton("anadir");
-		btnAdd.setBounds(580,140,100,50);
-		panel3.add(btnAdd);
-		
-		String titulos []= {"NO.contreol","Nombre","semestre","promedio","acciones"};
-		String elementos[][] = {
-				{"NO.contreol","Nombre","semestre","promedio","acciones"},
-				{"NO.contreol","Nombre","semestre","promedio","acciones"},
-				{"NO.contreol","Nombre","semestre","promedio","acciones"},
-				{"NO.contreol","Nombre","semestre","promedio","acciones"},
-				};
-		
-		
-		JTable tblMain = new JTable(elementos,titulos);
-		tblMain.setBounds(40,250,910,400);
-		
-		JScrollPane scrol = new JScrollPane(tblMain);
-		scrol.setBounds(40,250,100,50);
-		panel3.add(tblMain);
-		
-		
-		this.add(panel3);
-		
-		
-		
-	}
-	
-	
-	public void agregarRegistro()
-	{
-		panel2.setBounds(500,0,500,800);
-		panel2.setLayout(null);
-		panel2.setBackground(Color.yellow);
-		
-		JLabel lblRegistro = new JLabel();
-		Font fontIngresar = new Font(Font.MONOSPACED,Font.ITALIC,40);
-		 lblRegistro.setFont(fontIngresar);
-		 lblRegistro.setBounds(100,20,300,50);
-		 lblRegistro.setText("Registro");
-		 lblRegistro.setOpaque(true);
-		lblRegistro.setBackground(Color.blue);
-		 lblRegistro.setForeground(Color.pink);
-		 lblRegistro.setHorizontalAlignment(0);
-		panel2.add( lblRegistro);
-		
-		JLabel lblNombre = new JLabel();
-		Font fontRegistro = new Font(Font.MONOSPACED,Font.ITALIC,20);
-		 lblNombre.setFont(fontRegistro);
-		 lblNombre.setBounds(100,80,300,50);
-		 lblNombre.setText("ingresa tu nombre");
-		 lblNombre.setOpaque(true);
-		lblNombre.setBackground(Color.blue);
-		 lblNombre.setForeground(Color.pink);
-		 lblNombre.setHorizontalAlignment(0);
-		panel2.add( lblNombre);
-		
-		JTextField txtUsuario = new JTextField();
-		txtUsuario.setBounds(100,150,300,25);
-		panel2.add(txtUsuario);
-		
-		JLabel lblinfo = new JLabel();
-		lblinfo.setFont(fontRegistro);
-		lblinfo.setBounds(100,200,300,50);
-		lblinfo.setText("bio");
-		lblinfo.setOpaque(true);
-		lblinfo.setBackground(Color.blue);
-		lblinfo.setForeground(Color.pink);
-		lblinfo.setHorizontalAlignment(0);
-		panel2.add(lblinfo);
-		
-		JTextArea txtRegistro = new JTextArea();
-		txtRegistro.setBounds(100,280,300,80);
-		txtRegistro.setBorder(BorderFactory.createLineBorder(Color.black,2));
-		panel2.add(txtRegistro);
-		
-		JLabel lblpref = new JLabel("preferencias");
-		lblpref.setFont(fontRegistro);
-		lblpref.setBounds(100,350,300,50);
-		lblpref.setHorizontalAlignment(0);
-		panel2.add(lblpref);
-		
-		JCheckBox cbxPref = new JCheckBox();
-		cbxPref.setBounds(20,400,100,25);
-		cbxPref.setText("Duelces");
-		cbxPref.setOpaque(true);
-		cbxPref.setBackground(Color.blue);
-		cbxPref.setForeground(Color.pink);
-		panel2.add(cbxPref);
-		
-		JCheckBox cbxPref2 = new JCheckBox();
-		cbxPref2.setBounds(150,400,100,25);
-		cbxPref2.setText("saladp");
-		cbxPref2.setOpaque(true);
-		cbxPref2.setBackground(Color.blue);
-		cbxPref2.setForeground(Color.pink);
-		panel2.add(cbxPref2);
-		
-		JCheckBox cbxPref3 = new JCheckBox();
-		cbxPref3.setBounds(250,400,100,25);
-		cbxPref3.setText("saladuable");
-		cbxPref3.setOpaque(true);
-		cbxPref3.setBackground(Color.blue);
-		cbxPref3.setForeground(Color.pink);
-		cbxPref3.setBorderPainted(true);
-		cbxPref3.setBorder(BorderFactory.createLineBorder(Color.black,2));
-		panel2.add(cbxPref3);
-		
-		JLabel lblpref2 = new JLabel("Terminos y condicioes");
-		lblpref2.setFont(fontRegistro);
-		lblpref2.setBounds(100,450,300,50);
-		panel2.add(lblpref2);
-		
-		ButtonGroup grpTerms = new ButtonGroup();
-		
-		
-		JRadioButton rdnPref = new JRadioButton();
-		rdnPref.setBounds(20,500,100,25);
-		rdnPref.setText("acepto los terminos");
-		panel2.add(rdnPref);
-		
-		JRadioButton rdnPref2 = new JRadioButton();
-		rdnPref2.setBounds(150,500,100,25);
-		rdnPref2.setText("no acepto");
-		
-		panel2.add(rdnPref2);
-		
-		grpTerms.add(rdnPref);
-		grpTerms.add(rdnPref2);
-		
-		String[] elementos = {"asjhdkja","hola","sheck"};
-		JComboBox cmbx1 = new JComboBox(elementos);
-		cmbx1.setBounds(100,550,100,50);
-		panel2.add(cmbx1);
-		
-		JButton btnAceptar = new JButton();
-		btnAceptar.setBounds(150,620,100,50);
-		btnAceptar.setText("Crear");
-		panel2.add(btnAceptar);
-		
-		this.add(panel2);
-
-	}
-	
-	
-	
-	
-	
 	public void agregarElementos()
 	{
-		panel.setBounds(0,0,500,800);
+		panel.setBounds(0,-120,500,500);
 		panel.setLayout(null);
-		panel.setBackground(Color.red);
+		panel.setBackground(Color.LIGHT_GRAY);
 		
-		JLabel lblIngresar = new JLabel();
-		Font fontIngresar = new Font(Font.MONOSPACED,Font.ITALIC,40);
-		lblIngresar.setFont(fontIngresar);
-		lblIngresar.setBounds(100,20,300,50);
-		lblIngresar.setText("Acceder");
-		lblIngresar.setOpaque(true);
-		lblIngresar.setBackground(Color.blue);
-		lblIngresar.setForeground(Color.pink);
-		panel.add(lblIngresar);
+		Font font = new Font(Font.SANS_SERIF,Font.BOLD,25);
 		
-		JLabel lblUsuario = new JLabel();
-		Font fontUsuario = new Font(Font.MONOSPACED,Font.ITALIC,20);
-		lblUsuario.setFont(fontUsuario);
-		lblUsuario.setBounds(100,100,300,25);
-		lblUsuario.setText("Ingresa tu Usuario");
-		lblUsuario.setOpaque(true);
-		lblUsuario.setBackground(Color.blue);
-		lblUsuario.setForeground(Color.pink);
-		panel.add(lblUsuario);
+		JTextField txtMain = new JTextField();
+		txtMain.setBounds(10,150,400,50);
+		txtMain.setText("0");
+		txtMain.setFont(font);
+		txtMain.setHorizontalAlignment(0);	
+		txtMain.setBorder(BorderFactory.createLineBorder(Color.black));
+		panel.add(txtMain);
 		
-		JTextField txtUsuario = new JTextField();
-		txtUsuario.setBounds(100,150,300,25);
-		panel.add(txtUsuario);
+		JButton btnDiv = new JButton();
+		btnDiv.setBounds(310,200,100,50);
+		btnDiv.setText("/");
+		btnDiv.setBorderPainted(true);	
+		btnDiv.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnDiv.setOpaque(true);
+		btnDiv.setBackground(Color.white);
+		btnDiv.setFont(font);
+		btnDiv.setForeground(Color.blue);
+		panel.add(btnDiv);
 		
-		JLabel lblContrasena = new JLabel();
-		Font fontContrasena = new Font(Font.MONOSPACED,Font.ITALIC,20);
-		lblContrasena.setFont(fontContrasena);
-		lblContrasena.setBounds(100,200,300,25);
-		lblContrasena.setText("Ingresa tu contrasena");
-		lblContrasena.setOpaque(true);
-		lblContrasena.setBackground(Color.blue);
-		lblContrasena.setForeground(Color.pink);
-		panel.add(lblContrasena);
+		JButton btnMulti = new JButton();
+		btnMulti.setBounds(310,250,100,50);
+		btnMulti.setText("*");
+		btnMulti.setBorderPainted(true);	
+		btnMulti.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnMulti.setOpaque(true);
+		btnMulti.setBackground(Color.white);
+		btnMulti.setFont(font);
+		btnMulti.setForeground(Color.blue);
+		panel.add(btnMulti);
 		
-		JPasswordField pwUsuario = new JPasswordField();
-		pwUsuario.setBounds(100,250,300,25);
-		panel.add(pwUsuario);
+		JButton btnResta = new JButton();
+		btnResta.setBounds(310,300,100,50);
+		btnResta.setText("-");
+		btnResta.setBorderPainted(true);	
+		btnResta.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnResta.setOpaque(true);
+		btnResta.setBackground(Color.white);
+		btnResta.setForeground(Color.blue);
+		btnResta.setFont(font);
+		panel.add(btnResta);
 		
-		JCheckBox cbxGuardar = new JCheckBox();
-		cbxGuardar.setBounds(50,290,200,25);
-		cbxGuardar.setText("Guardar Contrasena");
-		panel.add(cbxGuardar);
+		JButton btnSuma = new JButton();
+		btnSuma.setBounds(310,350,100,50);
+		btnSuma.setText("+");
+		btnSuma.setBorderPainted(true);	
+		btnSuma.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnSuma.setOpaque(true);
+		btnSuma.setBackground(Color.white);
+		btnSuma.setForeground(Color.blue);
+		btnSuma.setFont(font);
+		panel.add(btnSuma);
+		
+		JButton btnPunto = new JButton();
+		btnPunto.setBounds(210,400,100,50);
+		btnPunto.setText(".");
+		btnPunto.setBorderPainted(true);	
+		btnPunto.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnPunto.setOpaque(true);
+		btnPunto.setBackground(Color.white);
+		btnPunto.setForeground(Color.blue);
+		btnPunto.setFont(font);
+		panel.add(btnPunto);
+		
+		JButton btnExpandir = new JButton();
+		btnExpandir.setBounds(10,400,100,50);
+		btnExpandir.setText("[]");
+		btnExpandir.setBorderPainted(true);	
+		btnExpandir.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnExpandir.setOpaque(true);
+		btnExpandir.setBackground(Color.white);
+		btnExpandir.setFont(font);
+		btnExpandir.setForeground(Color.blue);
+		panel.add(btnExpandir);
 		
 		JButton btnAceptar = new JButton();
-		btnAceptar.setBounds(150,320,100,50);
+		btnAceptar.setBounds(310,400,100,50);
 		btnAceptar.setText("Aceptar");
+		btnAceptar.setBorderPainted(true);	
+		btnAceptar.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnAceptar.setOpaque(true);
+		btnAceptar.setBackground(Color.white);
+		btnAceptar.setForeground(Color.blue);
+		btnAceptar.setFont(font);
 		panel.add(btnAceptar);
+
+		///////////////////////////////////
+		
+		JButton btnPorcentaje = new JButton();
+		btnPorcentaje.setBounds(210,200,100,50);
+		btnPorcentaje.setText("%");
+		btnPorcentaje.setBorderPainted(true);	
+		btnPorcentaje.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnPorcentaje.setOpaque(true);
+		btnPorcentaje.setBackground(Color.white);
+		btnPorcentaje.setForeground(Color.blue);
+		btnPorcentaje.setFont(font);
+		panel.add(btnPorcentaje);
+		
+		JButton btnBorrar = new JButton();
+		btnBorrar.setBounds(110,200,100,50);
+		btnBorrar.setText("<X]");
+		btnBorrar.setBorderPainted(true);	
+		btnBorrar.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnBorrar.setOpaque(true);
+		btnBorrar.setBackground(Color.white);
+		btnBorrar.setForeground(Color.blue);
+		btnBorrar.setFont(font);
+		panel.add(btnBorrar);
+		
+		JButton btnAC = new JButton();
+		btnAC.setBounds(10,200,100,50);
+		btnAC.setText("AC");
+		btnAC.setBorderPainted(true);	
+		btnAC.setBorder(BorderFactory.createLineBorder(Color.black));
+		btnAC.setOpaque(true);
+		btnAC.setBackground(Color.white);
+		btnAC.setFont(font);
+		btnAC.setForeground(Color.blue);
+		panel.add(btnAC);
+
+		
+		JButton btn9 = new JButton();
+		btn9.setBounds(210,250,100,50);
+		btn9.setText("9");
+		btn9.setBorderPainted(true);	
+		btn9.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn9.setOpaque(true);
+		btn9.setBackground(Color.white);
+		btn9.setFont(font);
+		panel.add(btn9);
+		
+		JButton btn8 = new JButton();
+		btn8.setBounds(110,250,100,50);
+		btn8.setText("8");
+		btn8.setBorderPainted(true);	
+		btn8.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn8.setOpaque(true);
+		btn8.setBackground(Color.white);
+		btn8.setFont(font);
+		panel.add(btn8);
+		
+		JButton btn7 = new JButton();
+		btn7.setBounds(10,250,100,50);
+		btn7.setText("7");
+		btn7.setBorderPainted(true);	
+		btn7.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn7.setOpaque(true);
+		btn7.setBackground(Color.white);
+		btn7.setFont(font);
+		panel.add(btn7);
+
+		
+		JButton btn6 = new JButton();
+		btn6.setBounds(210,300,100,50);
+		btn6.setText("6");
+		btn6.setBorderPainted(true);	
+		btn6.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn6.setOpaque(true);
+		btn6.setBackground(Color.white);
+		btn6.setFont(font);
+		panel.add(btn6);
+		
+		JButton btn5 = new JButton();
+		btn5.setBounds(110,300,100,50);
+		btn5.setText("5");
+		btn5.setBorderPainted(true);	
+		btn5.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn5.setOpaque(true);
+		btn5.setBackground(Color.white);
+		btn5.setFont(font);
+		panel.add(btn5);
+		
+		JButton btn4 = new JButton();
+		btn4.setBounds(10,300,100,50);
+		btn4.setText("4");
+		btn4.setBorderPainted(true);	
+		btn4.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn4.setOpaque(true);
+		btn4.setBackground(Color.white);
+		btn4.setFont(font);
+		panel.add(btn4);
+
+		
+		JButton btn3 = new JButton();
+		btn3.setBounds(210,350,100,50);
+		btn3.setText("3");
+		btn3.setBorderPainted(true);	
+		btn3.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn3.setOpaque(true);
+		btn3.setBackground(Color.white);
+		btn3.setFont(font);
+		panel.add(btn3);
+		
+		JButton btn2 = new JButton();
+		btn2.setBounds(110,350,100,50);
+		btn2.setText("2");
+		btn2.setBorderPainted(true);	
+		btn2.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn2.setOpaque(true);
+		btn2.setBackground(Color.white);
+		btn2.setFont(font);
+		panel.add(btn2);
+		
+		JButton btn1 = new JButton();
+		btn1.setBounds(10,350,100,50);
+		btn1.setText("1");
+		btn1.setBorderPainted(true);	
+		btn1.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn1.setOpaque(true);
+		btn1.setBackground(Color.white);
+		btn1.setFont(font);
+		panel.add(btn1);
 		
 		
+		JButton btn0 = new JButton();
+		btn0.setBounds(110,400,100,50);
+		btn0.setText("0");
+		btn0.setBorderPainted(true);	
+		btn0.setBorder(BorderFactory.createLineBorder(Color.black));
+		btn0.setOpaque(true);
+		btn0.setBackground(Color.white);
+		btn0.setFont(font);
+		panel.add(btn0);
 		
-		this.add(panel);
+		
 		
 	}
 
