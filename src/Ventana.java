03dcc1b715d7d0977ea855d3742a0f49c38d1742
sebastian123k <159938783+sebastian123k@users.noreply.github.com>
2024-03-05 06:34:34 +0100

    Unidad 2 actividad 28

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,16 +1,22 @@
-import javax.swing.JFrame;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.*;
 
 public class Ventana extends JFrame {
+	
 	
 	JPanel panel = new JPanel();
 	JPanel panel2 = new JPanel();
@@ -27,13 +33,29 @@ public class Ventana extends JFrame {
 	JPanel borde = new JPanel();
 	JPanel borde2 = new JPanel();
 	
+	JPanel bordeRojo = new JPanel();
+	JPanel bordeRojo2 = new JPanel();
+	
+	JPanel bordeVerde = new JPanel();
+	JPanel bordeVerde2 = new JPanel();
+	
+	JPanel panelRosa = new JPanel();
+	JPanel panelVerde2 = new JPanel();
+
+	JPanel panelDerecho = new JPanel();
+	JPanel panelIzquierdo = new JPanel();
+	
+	JPanel bord1 = new JPanel();
+	JPanel bord2 = new JPanel();
+	
 	public Ventana()
 	{	
-		this.setSize(450,570);
+		this.setSize(800,770);
 		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Calculando el interes");
 		this.setIconImage(new ImageIcon(getClass().getResource("icono.png")).getImage());
+		this.setLocationRelativeTo(null);
 		//this.agregarImagenes();
 		//this.agregarElementos();
 		//this.agregarRegistro();
@@ -48,140 +70,394 @@ public class Ventana extends JFrame {
 		
 		panelMain.setLayout(new BorderLayout());
 		panelMain.setBackground(Color.red);
-		panelMain.setSize(430,530);
-		panelMain.setBorder(BorderFactory.createLineBorder(Color.white,10));
+		panelMain.setSize(780,730);
+		panelMain.setBorder(BorderFactory.createLineBorder(new Color(0,255,0,255),10));
 		
-		JLabel lblInteres = new JLabel(" Interes");
-		lblInteres.setFont(new Font(Font.SANS_SERIF,Font.ITALIC,30));
-		lblInteres.setBackground(Color.white);
+		JLabel lblInteres = new JLabel("          Registro de usuarios");
+		lblInteres.setFont(new Font(Font.SANS_SERIF,Font.ITALIC,20));
+		lblInteres.setBackground(new Color(0,255,0,255));
 		lblInteres.setForeground(Color.red);
 		lblInteres.setOpaque(true);
 		panelMain.add(lblInteres,BorderLayout.NORTH);
 		
 		////////////////////////
-		/////panelcenthro////
+		
+		/////parte izquierda////
+		
+		panelIzquierdo.setLayout(new BorderLayout());
+		panelIzquierdo.setBackground(new Color(100,250,200));
+		panelIzquierdo.setSize(580,680);
+		panelIzquierdo.setBorder(BorderFactory.createLineBorder(new Color(0,255,0,255),10));
+		panelMain.add(panelIzquierdo,BorderLayout.CENTER);
 		
 		centro.setLayout(new BorderLayout());
-		centro.setBackground(new Color(156,249,156));
+		centro.setBackground(new Color(102,255,204));
 		centro.setSize(580,680);
-		centro.setBorder(BorderFactory.createLineBorder(Color.white,10));
-		panelMain.add(centro,BorderLayout.CENTER);
+		centro.setBorder(BorderFactory.createLineBorder(new Color(0,255,0,255),10));
+		panelIzquierdo.add(centro,BorderLayout.CENTER);
 		
 		
 		borde.setLayout(new BorderLayout());
-		borde.setBackground(new Color(156,249,156));
-		borde.setBorder(BorderFactory.createLineBorder(new Color(156,249,156),10));
+		borde.setBackground(new Color(100,250,200));
+		borde.setBorder(BorderFactory.createLineBorder(new Color(100,250,200),10,true));
 		centro.add(borde,BorderLayout.CENTER);
 		
 
 		borde2.setLayout(new BorderLayout());
-		borde2.setBackground(new Color(156,249,156));
-		borde2.setBorder(BorderFactory.createLineBorder(Color.gray,2));
+		borde2.setBackground(new Color(100,250,200));
+		borde2.setBorder(BorderFactory.createLineBorder(Color.gray,2,true));
 		borde.add(borde2,BorderLayout.CENTER);
 		
-		JLabel lblCalcular = new JLabel("  Calcular Interes");
+		JLabel lblCalcular = new JLabel("  Datos Generales");
 		lblCalcular.setFont(new Font(Font.SANS_SERIF,Font.BOLD,15));
-		lblCalcular.setBackground(new Color(156,249,156));
+		lblCalcular.setBackground(new Color(0,0,0,1));
 		lblCalcular.setForeground(Color.black);
 		lblCalcular.setOpaque(true);
 		centro.add(lblCalcular,BorderLayout.NORTH);
+	
+		GridBagConstraints gbc = new GridBagConstraints();
 		
-		panelVerde.setLayout(new GridLayout(4,2));
-		panelVerde.setBackground(new Color(156,249,156));
-		panelVerde.setBorder(BorderFactory.createLineBorder(new Color(156,249,156),10));
+		panelVerde.setLayout(new GridBagLayout());
+		panelVerde.setBackground(new Color(100,250,200));
 		borde2.add(panelVerde,BorderLayout.CENTER);
 		
 		
-		JLabel lblCapital = new JLabel("Capital:");
+		gbc.insets = new Insets(20,0,0,50);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		JLabel lblCapital = new JLabel("Nombres:");
 		lblCapital.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
-		lblCapital.setBackground(new Color(156,249,156));
+		lblCapital.setBackground(new Color(0,0,0,1));
 		lblCapital.setForeground(Color.black);
 		lblCapital.setOpaque(true);
-		lblCapital.setHorizontalAlignment(0);
-		panelVerde.add(lblCapital);
+		panelVerde.add(lblCapital,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 0;
 		
 		JTextField txtfldCapital = new JTextField();
-		txtfldCapital.setBorder(BorderFactory.createLineBorder(new Color(156,249,156),20));
-		txtfldCapital.setText("  1500");
-		panelVerde.add(txtfldCapital);
+		txtfldCapital.setText("  Juan Carlos");
+		txtfldCapital.setPreferredSize(new Dimension(140,25));
+		panelVerde.add(txtfldCapital,gbc);
 		
-		JLabel lblTiempo = new JLabel("Tiempo:");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		
+		JLabel lblTiempo = new JLabel("Apellido Paterno:");
 		lblTiempo.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
-		lblTiempo.setBackground(new Color(156,249,156));
+		lblTiempo.setBackground(new Color(0,0,0,1));
 		lblTiempo.setForeground(Color.black);
 		lblTiempo.setOpaque(true);
 		lblTiempo.setHorizontalAlignment(0);
-		panelVerde.add(lblTiempo);
+		panelVerde.add(lblTiempo,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 1;
 		
 		JTextField txtfldTiempo = new JTextField();
-		txtfldTiempo.setBorder(BorderFactory.createLineBorder(new Color(156,249,156),20));
-		txtfldTiempo.setText("  2");
-		panelVerde.add(txtfldTiempo);
+		txtfldTiempo.setText("  Arcilla");
+		txtfldTiempo.setPreferredSize(new Dimension(140,25));
+		panelVerde.add(txtfldTiempo,gbc);
 		
-		JLabel lblTasa = new JLabel("             Tasa de interes:");
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		
+		JLabel lblTasa = new JLabel("Apellido Materno:");
 		lblTasa.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
-		lblTasa.setBackground(new Color(156,249,156));
+		lblTasa.setBackground(new Color(0,0,0,1));
 		lblTasa.setForeground(Color.black);
 		lblTasa.setOpaque(true);
 		lblTasa.setHorizontalAlignment(0);
-		panelVerde.add(lblTasa);
+		panelVerde.add(lblTasa,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 2;
 		
 		JTextField txtfldTasa = new JTextField();
-		txtfldTasa.setBorder(BorderFactory.createLineBorder(new Color(156,249,156),20));
-		txtfldTasa.setText("  0.1");
-		panelVerde.add(txtfldTasa);
+		txtfldTasa.setText("  Diaz");
+		txtfldTasa.setPreferredSize(new Dimension(140,25));
+		panelVerde.add(txtfldTasa,gbc);
 		
-		JLabel imgCalcular = new JLabel();
-		imgCalcular.setIcon(new ImageIcon(getClass().getResource("botonsote.jpeg")));
-		imgCalcular.setBounds(330,530,100,32);
-		imgCalcular.setHorizontalAlignment(0);
-		panelVerde.add(imgCalcular);
+		gbc.gridx = 0;
+		gbc.gridy = 3;
 		
-		JLabel imgCancelar = new JLabel();
-		imgCancelar.setIcon(new ImageIcon(getClass().getResource("botonsillo.jpeg")));
-		imgCancelar.setBounds(330,530,100,32);
-		imgCancelar.setHorizontalAlignment(0);
-		panelVerde.add(imgCancelar);
+		JLabel lblFecha = new JLabel("fecha de nacimiento");
+		lblFecha.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
+		lblFecha.setBackground(new Color(0,0,0,1));
+		lblFecha.setForeground(Color.black);
+		lblFecha.setOpaque(true);
+		lblFecha.setHorizontalAlignment(0);
+		panelVerde.add(lblFecha,gbc);
 		
+		gbc.gridx = 1;
+		gbc.gridy = 3;
 		
+		JLabel imgFecha = new JLabel();
+		imgFecha.setIcon(new ImageIcon(getClass().getResource("fecha.jpeg")));
+		imgFecha.setHorizontalAlignment(0);
+		panelVerde.add(imgFecha,gbc);
 		
+		gbc.gridx = 0;
+		gbc.gridy = 4;
 		
+		JLabel lblSexo = new JLabel("Sexo");
+		lblSexo.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
+		lblSexo.setBackground(new Color(0,0,0,1));
+		lblSexo.setForeground(Color.black);
+		lblSexo.setOpaque(true);
+		lblSexo.setHorizontalAlignment(0);
+		panelVerde.add(lblSexo,gbc);
 		
-		////panelsur///
+		gbc.gridx = 1;
+		gbc.gridy = 4;
 		
+		ButtonGroup btngSexo = new ButtonGroup();
+	
+		JRadioButton rdnMasculino = new JRadioButton("Masculino");
+		rdnMasculino.setBackground(new Color(0,0,0,1));
+		btngSexo.add(rdnMasculino);
 		
-		panelRojo.setLayout(new GridLayout(2,2));
-		panelRojo.setBackground(new Color(251,157,156));
-		panelRojo.setBorder(BorderFactory.createLineBorder(Color.white,10));
-		panelMain.add(panelRojo,BorderLayout.SOUTH);
+		JRadioButton rdnFemenino = new JRadioButton("Femenino");
+		rdnFemenino.setBackground(new Color(0,0,0,1));		
+		btngSexo.add(rdnFemenino);
+		
+		panelVerde.add(rdnMasculino,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		
+		panelVerde.add(rdnFemenino,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		
+		JLabel lblNaci = new JLabel("Nacionalidad");
+		lblNaci.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
+		lblNaci.setBackground(new Color(0,0,0,1));
+		lblNaci.setForeground(Color.black);
+		lblNaci.setOpaque(true);
+		lblNaci.setHorizontalAlignment(0);
+		panelVerde.add(lblNaci,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 6;
+		
+		String[] elementos = {"peru                                ","hola","sheck"};
+		JComboBox cmbx1 = new JComboBox(elementos);
+		panelVerde.add(cmbx1,gbc);
+		//////////////////////////////
+		//////////////////////////
+		/////////////////////////////////////////////
+	////////////////////////////////////////////
+		////panelsur izquierdo///
+		
+		bord1.setLayout(new GridLayout(1,1));
+		bord1.setBackground(new Color(251,157,156));
+		bord1.setBorder(BorderFactory.createLineBorder(new Color(0,255,0,255),10));
+		panelIzquierdo.add(bord1,BorderLayout.SOUTH);
+		
+		bord2.setLayout(new BorderLayout());
+		bord2.setBackground(new Color(255,102,102));
+		bord2.setBorder(BorderFactory.createLineBorder(new Color(255,102,102),10));
+		bord1.add(bord2);
+		
+		JLabel lblDatos = new JLabel("Datos Opcionales");
+		lblDatos.setFont(new Font(Font.SANS_SERIF,Font.BOLD,15));
+		lblDatos.setBackground(new Color(255,102,102));
+		lblDatos.setForeground(Color.black);
+		lblDatos.setOpaque(true);
+		bord2.add(lblDatos,BorderLayout.NORTH);
+		
+		panelRojo.setLayout(new GridBagLayout());
+		panelRojo.setBackground(new Color(255,102,102));
+		panelRojo.setBorder(BorderFactory.createLineBorder(Color.red,2));
+		bord2.add(panelRojo,BorderLayout.CENTER);
+		
+		GridBagConstraints gbc4 = new GridBagConstraints();
+		
+		gbc4.insets = new Insets(0,0,0,0);
+		
+		gbc4.gridx = 0;
+		gbc4.gridy = 0;
 		
 		JLabel lblInteres2 = new JLabel("Interes:");
 		lblInteres2.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
-		lblInteres2.setBackground(new Color(251,157,156));
+		lblInteres2.setBackground(new Color(0,0,0,1));
 		lblInteres2.setForeground(Color.black);
-		lblInteres2.setBorder(BorderFactory.createLineBorder(new Color(251,157,156),25));
 		lblInteres2.setOpaque(true);
 		lblInteres2.setHorizontalAlignment(0);
-		panelRojo.add(lblInteres2);
+		panelRojo.add(lblInteres2,gbc4);
 		
-		JTextField txtfldInteres = new JTextField();
-		txtfldInteres.setBorder(BorderFactory.createLineBorder(new Color(251,157,156),20));
-		txtfldInteres.setText("  315.000000002");
-		panelRojo.add(txtfldInteres);
+		gbc4.gridx = 2;
+		gbc4.gridy = 0;
 		
-		JLabel lblMonto = new JLabel("Monto:");
-		lblMonto.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
-		lblMonto.setBackground(new Color(251,157,156));
-		lblMonto.setForeground(Color.black);
-		lblMonto.setBorder(BorderFactory.createLineBorder(new Color(251,157,156),25));
-		lblMonto.setOpaque(true);
-		lblMonto.setHorizontalAlignment(0);
-		panelRojo.add(lblMonto);
+		JLabel lblNada = new JLabel("      ");
+		lblNada.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
+		lblNada.setBackground(new Color(0,0,0,1));
+		lblNada.setForeground(Color.black);
+		lblNada.setOpaque(true);
+		lblNada.setHorizontalAlignment(0);
+		panelRojo.add(lblNada,gbc4);
 		
-		JTextField txtfldMonto = new JTextField();
-		txtfldMonto.setBorder(BorderFactory.createLineBorder(new Color(251,157,156),20));
-		txtfldMonto.setText("  1815.000000002");
-		panelRojo.add(txtfldMonto);
+		gbc4.gridx = 3;
+		gbc4.gridy = 0;
+		
+		JLabel lblPref = new JLabel("preferencias");
+		lblPref.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,15));
+		lblPref.setBackground(new Color(0,0,0,1));
+		lblPref.setForeground(Color.black);
+		lblPref.setBorder(BorderFactory.createLineBorder(new Color(0,0,0,1),8));
+		lblPref.setOpaque(true);
+		lblPref.setHorizontalAlignment(0);
+		panelRojo.add(lblPref,gbc4);
+		
+		
+
+		
+		gbc4.gridx = 0;
+		gbc4.gridy = 1;
+		
+		JTextArea cmbxHola = new JTextArea("Hola amigos \nde youtube gracias \npor compartir\nmis videos");
+		cmbxHola.setPreferredSize(new Dimension(150,100));
+		JScrollPane sbHola = new JScrollPane(cmbxHola);
+		
+		sbHola.setPreferredSize(new Dimension(150,20));
+		sbHola.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		JScrollPane sbHola2 = new JScrollPane(cmbxHola);
+		sbHola2.setPreferredSize(new Dimension(20,100));
+		sbHola2.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		panelRojo.add(cmbxHola,gbc4);
+		
+		gbc4.gridx = 0;
+		gbc4.gridy = 2;
+		
+		panelRojo.add(sbHola,gbc4);
+	
+		gbc4.gridx = 1;
+		gbc4.gridy = 1;
+		
+		panelRojo.add(sbHola2,gbc4);
+		
+		gbc4.gridx = 3;
+		gbc4.gridy = 1;
+		
+		
+		JTextArea cmbxOpciones = new JTextArea("  Cantar \n  escuchar musica \n  Leer \n  Deportes \n  Otros");
+		cmbxOpciones.setPreferredSize(new Dimension(150,100));
+		JScrollPane sbHola3 = new JScrollPane(cmbxOpciones);
+		sbHola3.setPreferredSize(new Dimension(20,100));
+		sbHola3.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		panelRojo.add(cmbxOpciones,gbc4);
+		
+		gbc4.gridx = 4;
+		gbc4.gridy = 1;
+		
+		
+		panelRojo.add(sbHola3,gbc4);
+		
+		
+		
+		////////////////////////////parte derecha
+		///////////////////////////////
+		///////////////////////////////////
+		////////////////////////
+		///////////////////
+		
+		
+		panelDerecho.setLayout(new BorderLayout());
+		panelDerecho.setBackground(new Color(255,153,153));
+		
+		panelMain.add(panelDerecho,BorderLayout.EAST);
+		
+		
+		
+		bordeRojo.setLayout(new GridLayout(1,1));
+		bordeRojo.setBackground(new Color(251,157,156));
+		bordeRojo.setBorder(BorderFactory.createLineBorder(new Color(0,255,0,255),10));
+		panelDerecho.add(bordeRojo,BorderLayout.CENTER);
+		
+		bordeRojo2.setLayout(new BorderLayout());
+		bordeRojo2.setBackground(new Color(251,157,156));
+		bordeRojo2.setBorder(BorderFactory.createLineBorder(new Color(251,157,156),10));
+		bordeRojo.add(bordeRojo2,BorderLayout.CENTER);
+		
+		JLabel lblUsuario = new JLabel("Perfil de usuario");
+		lblUsuario.setFont(new Font(Font.SANS_SERIF,Font.BOLD,15));
+		lblUsuario.setBackground(new Color(251,157,156));
+		lblUsuario.setForeground(Color.black);
+		lblUsuario.setOpaque(true);
+		bordeRojo2.add(lblUsuario,BorderLayout.NORTH);
+		
+		GridBagConstraints gbc2 = new GridBagConstraints();
+		
+		panelRosa.setLayout(new GridBagLayout());
+		panelRosa.setBackground(new Color(255,153,153));
+		panelRosa.setBorder(BorderFactory.createLineBorder(Color.red,2));
+		bordeRojo2.add(panelRosa,BorderLayout.CENTER);
+		
+		gbc2.insets = new Insets(10,10,10,10);
+		gbc2.gridx = 0;
+		gbc2.gridy = 0;
+		
+		JLabel imgFoto = new JLabel();
+		imgFoto.setIcon(new ImageIcon(getClass().getResource("foto.jpeg")));
+		imgFoto.setBounds(330,530,100,32);
+		imgFoto.setHorizontalAlignment(0);
+		panelRosa.add(imgFoto,gbc2);
+		
+		gbc2.gridx = 0;
+		gbc2.gridy = 1;
+		
+		JCheckBox cbxFoto = new JCheckBox("Mostra Foto de perfil");
+		cbxFoto.setBackground(new Color(0,0,0,1));
+		panelRosa.add(cbxFoto,gbc2);
+		
+		
+		gbc2.gridx = 0;
+		gbc2.gridy = 2;
+		
+		JCheckBox cbxFecha = new JCheckBox("Mostra Fecha de nacimiento");
+		cbxFecha.setBackground(new Color(0,0,0,1));
+		panelRosa.add(cbxFecha,gbc2);
+		
+		
+		///panel sur derecho
+		
+		GridBagConstraints gbc3 = new GridBagConstraints();
+		gbc3.insets = new Insets(20,10,20,10);
+		
+		panelVerde2.setLayout(new GridBagLayout());
+		panelVerde2.setBackground(new Color(102,255,204));
+		panelVerde2.setBorder(BorderFactory.createLineBorder(new Color(0,255,0,255),10));
+		panelDerecho.add(panelVerde2,BorderLayout.SOUTH);
+		
+		gbc3.gridx = 0;
+		gbc3.gridy = 0;
+		
+		JLabel imgCancelar = new JLabel();
+		imgCancelar.setIcon(new ImageIcon(getClass().getResource("botonsillo.jpeg")));
+		panelVerde2.add(imgCancelar,gbc3);
+		
+		gbc3.gridx = 0;
+		gbc3.gridy = 1;
+		
+		JLabel imgCalcular = new JLabel();
+		imgCalcular.setIcon(new ImageIcon(getClass().getResource("botonsote.jpeg")));
+		panelVerde2.add(imgCalcular,gbc3);
+		
+		gbc3.gridx = 0;
+		gbc3.gridy = 2;
+		
+		JLabel imgSalir = new JLabel();
+		imgSalir.setIcon(new ImageIcon(getClass().getResource("salir.jpeg")));
+		panelVerde2.add(imgSalir,gbc3);
+		
+		
+		
+		
 		
 		
 		
