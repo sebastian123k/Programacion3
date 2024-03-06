eab27a6b6c59df059cbf0a0c996f3778d66d4425
sebastian123k <159938783+sebastian123k@users.noreply.github.com>
2024-03-06 05:21:14 +0100

    Unidad Actividad 31

--- src/Ventana.java
+++ src/Ventana.java
@@ -32,30 +32,84 @@ public class Ventana extends JFrame{
 		Graphics g2d = (Graphics2D) g;
 		
 		g2d.setColor(Color.BLUE);
+		g2d.fillRect(0, 0, 700, 700);
 		
-		g2d.fillRect(100, 100, 100, 100);
-		g2d.drawLine(30, 70, 770, 70);
+		g2d.setColor(Color.GRAY);
+		g2d.fillRect(430, 160, 70, 100);
 		
-		g2d.drawArc(300,300,45,45,45,180);
-		g2d.drawArc(300,300,45,45,45,180);
+		g2d.setColor(Color.GRAY);
+		g2d.fillRect(420, 120, 90, 40);
 		
-		g2d.drawOval(100, 200, 50, 100);
-		g2d.fillOval(300, 200, 50, 100);
+		g2d.setColor(Color.BLACK);
+		int x[] = {430,500,500};
+		int y[] = {160,160,190};
+		g2d.fillPolygon(new Polygon(x,y,3));
 		
-		g2d.drawPolygon(new Polygon());
-		((Graphics2D) g2d).setStroke(new BasicStroke(10));
-		g2d.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
-		g2d.drawString("holanda",400,100);
+		g2d.setColor(Color.orange);
+		g2d.fillRect(0, 400, 700, 100);
 		
-		g2d.drawRoundRect(300,300 , 100, 50, 100, 100);
+		for(int xi = 10;xi<700; xi+=60)
+		{
 		
-		try {
-			BufferedImage image = ImageIO.read(new File("src/spider.png"));
-			g2d.drawImage(image,0,0,null);
-		} catch (IOException e) {
+			g2d.setColor(Color.orange);
+			g2d.fillRect(xi,360, 50, 250);
 			
-			e.printStackTrace();
+			int x3[] = {xi,xi+25,xi+50};
+			int y3[] = {360,320,360};
+			g2d.fillPolygon(new Polygon(x3,y3,3));
+				
 		}
+		
+		g2d.setColor(Color.RED);
+		int x2[] = {150,290,590};
+		int y2[] = {300,110,300};
+		g2d.fillPolygon(new Polygon(x2,y2,3));
+		
+		
+		g2d.setColor(Color.yellow);
+		g2d.fillRect(190, 300, 350, 270);
+		
+		for(int yi = 335;yi<570; yi+=40)
+		{
+			g2d.setColor(Color.red);
+			g2d.drawLine(190, yi, 540, yi);
+			
+			g2d.setColor(Color.red);
+			g2d.drawLine(190, yi+1, 540, yi+1);
+			
+			g2d.setColor(Color.red);
+			g2d.drawLine(190+2, yi+2, 540, yi+2);
+				
+		}
+		
+		g2d.setColor(Color.gray);
+		g2d.fillRect(160, 570, 410, 40);
+		
+		g2d.setColor(Color.black);
+		g2d.fillRect(230, 320, 140, 250);
+		
+		g2d.setColor(Color.GRAY);
+		g2d.fillRect(240, 330, 120, 230);
+		
+		g2d.setColor(Color.PINK);
+		g2d.fillRoundRect(330,450,10,10,90,90);
+		
+		g2d.setColor(Color.red);
+		g2d.fillRect(410, 320, 100, 100);
+		
+		g2d.setColor(Color.white);
+		g2d.fillRect(420, 330, 80, 80);
+		
+		g2d.setColor(Color.red);
+		g2d.fillRect(450, 320, 10,90);
+		
+		g2d.setColor(Color.red);
+		g2d.fillRect(410, 360, 90,10);
+		
+		g2d.setColor(Color.green);
+		g2d.fillRect(0, 600, 700, 300);
+		
+		
 		
 		
 	}
