608cf8025534a2ddb77fc7b56c27aa0e70501b0b
sebastian123k <159938783+sebastian123k@users.noreply.github.com>
2024-03-09 02:56:50 +0100

    Unidad 2 Actividad 34

--- src/Ventana.java
+++ src/Ventana.java
@@ -36,199 +36,201 @@ public class Ventana extends JFrame implements MouseListener{
 		Graphics g2d = (Graphics2D) g;
 		
 		//fondo
-		g2d.setColor(new Color(176,232,250));
+		g2d.setColor(new Color(6,100,255));
 		g2d.fillRect(0,0,1000,700);
+		
+		
+		/// nubes traceras//
+		
+		g2d.setColor(new Color(235,244,255));
+		
+		g2d.fillRoundRect(260,460,240,50,100,50);
+		
+		g2d.fillRoundRect(160,410,240,50,100,50);
+		
+		g2d.fillRoundRect(260,360,240,50,100,50);
+		
+		g2d.fillRoundRect(780,130,200,50,100,50);
+		
+		g2d.fillRoundRect(650,180,240,50,100,50);
+		
+		//montanilas
+		
+		g2d.setColor(new Color(198,227,255));
+		g2d.fillRoundRect(20,150,200,500,500,200);
+		
+		g2d.fillRoundRect(380,150,200,500,500,200);
+		
+		g2d.fillRoundRect(585,80,200,700,500,200);
+		
+		//nubes2
+		
+		g2d.setColor(Color.white);
+		
+		g2d.fillRoundRect(470,310,240,50,100,50);
+		
+		
+		//montanilas de frente
+		
+				g2d.setColor(new Color(118,178,215));
+				
+				g2d.fillRoundRect(-70,350,200,300,500,200);
+				
+				g2d.fillRoundRect(350,350,250,300,500,200);
+				
+				g2d.fillRoundRect(600,250,250,400,600,200);
+				
+		//nubes de frente
+				
+				g2d.setColor(Color.white);
+				
+				g2d.fillRoundRect(-60,230,240,50,100,50);
+				
+				g2d.fillRoundRect(220,280,240,50,100,50);
+
+		
 		
 		//arbusto
 		
-		g2d.setColor(Color.BLACK);
-		g2d.fillOval(85, 382, 130, 120);
+				g2d.setColor(Color.BLACK);
+				g2d.fillOval(115, 455, 190, 120);
 
-		g2d.fillOval(65, 432, 130, 120);
-	
-		g2d.fillOval(105, 442, 130, 120);
-
-	
-
-		g2d.setColor(new Color(78,197,98));
-		g2d.fillOval(100, 400, 100, 100);
+				g2d.setColor(new Color(4,252,13));
+				g2d.fillOval(120, 460, 180, 100);
+				
+		//bolas de las monta;as
 		
-		g2d.fillOval(80, 440, 100, 100);
+		g2d.setColor(Color.white);
+		g2d.fillOval(160,200,20,40);
+		g2d.fillOval(160,300,20,40);
+		g2d.fillOval(100,320,20,40);
 		
-		g2d.fillOval(120, 450, 100, 100);
+		g2d.fillOval(470,200,20,40);
+		g2d.fillOval(420,250,20,40);
 		
-		//otra parte del suelo
-		g2d.setColor(Color.black);
-		g2d.fillRect(0, 500, 1000, 200);
+		g2d.fillOval(670,100,20,40);
+		g2d.fillOval(620,150,20,40);
 		
-		g2d.setColor(new Color(249,199,192));
-		g2d.fillRect(0, 505, 1000, 20);
-		
-		
-		///////////////// plataformarosa y azul
+		g2d.fillOval(720,300,20,40);
+		g2d.fillOval(770,450,20,40);
+				
 		
 		
-		g2d.setColor(Color.BLACK);
-		g2d.fillRoundRect(380, 250, 180, 250,10,10);
+		
+		
 
 		
-		g2d.setColor(Color.BLACK);
-		g2d.fillRect(430, 290, 160, 210);
-		
-		g2d.setColor(new Color(132,191,221));
-		g2d.fillRect(385, 255, 170, 245);
-		
-		g2d.setColor(new Color(31,142,221));
-		g2d.fillRect(555, 255, 5, 245);
-		
-		g2d.setColor(Color.BLACK);
-		g2d.fillRoundRect(300, 350, 150, 150,10,10);
-		
-		g2d.setColor(Color.BLACK);
-		g2d.fillRect(320, 370, 160, 130);
-		
-		g2d.setColor(new Color(255,195,184));
-		g2d.fillRect(305, 355, 140, 145);
 		
 		
 		//////////////// bloques de ojetos
+				
+		g2d.setColor(Color.black);
+		g2d.fillRoundRect(845, 345, 50, 50,5,5);
+		
+		g2d.setColor(Color.gray);
+		g2d.fillRoundRect(850, 350, 40, 40,5,5);
 		
 		g2d.setColor(Color.black);
-		g2d.fillRect(65, 245, 70, 70);
+		g2d.fillRoundRect(845, 395, 50, 50,5,5);
 		
-		g2d.setColor(new Color(254,144,98));
-		g2d.fillRect(70, 250, 60, 60);
-		
-		g2d.setColor(Color.black);
-		g2d.fillRect(177, 117, 127, 67);
-		
-		g2d.setColor(new Color(254,144,98));
-		g2d.fillRect(242, 120, 60, 60);
-		
-		g2d.fillRect(180, 120, 60, 60);
+		g2d.setColor(Color.gray);
+		g2d.fillRoundRect(850, 400, 40, 40,5,5);
 		
 		g2d.setColor(Color.black);
-		g2d.fillRect(897, 187, 65, 65);
+		g2d.fillRoundRect(845, 445, 50, 50,5,5);
 		
-		g2d.setColor(new Color(254,144,98));
-		g2d.fillRect(900, 190, 60, 60);
+		g2d.setColor(Color.gray);
+		g2d.fillRoundRect(850, 450, 40, 40,5,5);
+		
+		g2d.setColor(Color.black);
+		g2d.fillRoundRect(895, 345, 50, 50,5,5);
+		
+		g2d.setColor(new Color(253,226,42));
+		g2d.fillRoundRect(900, 350, 40, 40,5,5);
+		
+		g2d.setColor(Color.black);
+		g2d.fillRoundRect(945, 345, 50, 50,5,5);
+		
+		g2d.setColor(new Color(253,226,42));
+		g2d.fillRoundRect(950, 350, 40, 40,5,5);
+		
 		
 	
 		
 		//tuberia
 		g2d.setColor(Color.black);
-		g2d.fillRect(670, 360, 130, 140);
+		g2d.fillRect(420, 410, 130, 140);
 		
-		g2d.setColor(new Color(24,136,17));
-		g2d.fillRect(675, 370, 120, 130);
+		g2d.setColor(new Color(78,77,140));
+		g2d.fillRect(425, 420, 120, 130);
 		
 		g2d.setColor(Color.black);
-		g2d.fillRect(660, 310, 150, 60);
+		g2d.fillRect(410, 360, 150, 60);
 		
-		g2d.setColor(new Color(24,136,17));
-		g2d.fillRect(665, 315, 140, 55);
+		g2d.setColor(new Color(78,77,140));
+		g2d.fillRect(415, 365, 140, 55);
 		
-		g2d.setColor(new Color(44,172,50));
+		g2d.setColor(new Color(132,131,204));
 		
-		g2d.fillRect(680, 315, 5, 185);
+		g2d.fillRect(430, 365, 5, 185);
 
-		g2d.fillRect(690, 315, 5, 185);
+		g2d.fillRect(440, 365, 5, 185);
 
-		g2d.fillRect(695, 315, 5, 185);
+		g2d.fillRect(445, 365, 5, 185);
 		
-		g2d.fillRect(710, 315, 5, 185);
+		g2d.fillRect(470, 365, 5, 185);
 		
-		g2d.fillRect(750, 315, 5, 185);
+		g2d.fillRect(500, 365, 5, 185);
 		
-		g2d.fillRect(765, 315, 5, 185);
+		g2d.fillRect(515, 365, 5, 185);
 		
-		g2d.fillRect(775, 315, 5, 185);
+		g2d.fillRect(525, 365, 5, 185);
 		
-		g2d.fillRect(787, 315, 5, 185);
+		g2d.fillRect(537, 365, 5, 185);
 		
 		g2d.setColor(Color.BLACK);
 		
-		g2d.fillRect(660, 365, 145, 5);
+		g2d.fillRect(410, 415, 145, 5);
 		
 		
 		
-		//plataforma verde
 		
+		//otra parte del suelo
 		g2d.setColor(Color.black);
-		g2d.fillRect(845, 295, 210, 205);
+		g2d.fillRect(0, 495, 1000, 200);
 		
-		g2d.setColor(new Color(82,216,109));
-		g2d.fillRect(850, 300, 200, 200);
-		
-		
-		//tuercas de las palataformas 
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(318, 368, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(320, 370, 15, 15);
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(408, 368, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(410, 370, 15, 15);
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(318, 468, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(320, 470, 15, 15);
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(408, 468, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(410, 470, 15, 15);
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(408, 268, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(410, 270, 15, 15);
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(518, 268, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(520, 270, 15, 15);
-		//
-		g2d.setColor(Color.black );
-		g2d.fillOval(868, 328, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(870, 330, 15, 15);
-		
-		g2d.setColor(Color.black );
-		g2d.fillOval(868, 468, 20,20);
-		
-		g2d.setColor(Color.gray);
-		g2d.fillOval(870, 470, 15, 15);
+		g2d.setColor(new Color(205,156,97));
+		g2d.fillRect(0, 510, 1000, 200);
 		
 		
 		// suelo
 		
-		int avance = 0;
-		for(int y =0; y<50;y+=5)
+		for(double i = 0.0;i<1200;i+=2)
 		{
-			for(int x =0; x<1000;x+=40)
-			{
-				g2d.setColor(new Color(45,6,0));
-				g2d.fillRect(x+avance,y+530, 20, 20);
-				
-				g2d.setColor(new Color(227,151,135));
-				g2d.fillRect(x+avance+20,y+530, 20, 5);
-			}
-			
-			avance+=5;
-			
+			g2d.setColor(Color.black);
+			g2d.fillRect(((int)i),513,6,(int)(Math.cos(i)*10));
+			System.out.println((int)(Math.cos(i)*10));
 		}
+		
+		for(double i = 0.0;i<1200;i+=2)
+		{
+			g2d.setColor(new Color(22,154,27));
+			g2d.fillRect(((int)i),510,6,(int)(Math.cos(i)*10));
+			System.out.println((int)(Math.cos(i)*10));
+		}
+		
+		for(int i = 0; i<1000;i++)
+		{
+			g2d.setColor(new Color(222,178,82));
+			g2d.fillOval(i*20+i*2,530,10,4+(int)(Math.cos(i)*10));
+			
+			g2d.fillOval(i*20+i*3,560,10,4+(int)(Math.sin(i)*20));
+		}
+		
+		
+		
+		g2d.setColor(new Color(22,154,27));
+		g2d.fillRect(0, 497, 1000, 10);
 		
 		
 		
@@ -268,8 +270,8 @@ public class Ventana extends JFrame implements MouseListener{
 				
 				};
 		
-		int posx = 550;
-		int posy = 445;
+		int posx = 380;
+		int posy = 440;
 		
 		for(int x = 0;x<15;x++)
 		{
@@ -302,8 +304,6 @@ public class Ventana extends JFrame implements MouseListener{
 		
 		//planta pirania 
 		
-		g2d.setColor(Color.green);
-		g2d.fillRect(730, 230, 10, 80);
 		
 		int planta[][] ={
 				{0,1,1,1,0,0,0,0,0,0,0,0,1,1,1,0},
@@ -320,16 +320,22 @@ public class Ventana extends JFrame implements MouseListener{
 				{0,1,2,2,2,2,4,4,2,2,4,2,2,2,1,0},
 				{0,1,2,4,4,2,2,2,2,2,2,2,2,2,1,0},
 				{0,0,1,1,4,2,2,2,2,2,2,2,1,1,0,0},
-				{0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0},
-				
+				{0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0},
+				{0,0,0,0,1,1,3,3,3,3,3,1,1,0,0,0},
+				{0,0,0,0,1,3,3,3,3,3,3,3,1,0,0,0},
+				{0,0,1,1,3,3,3,1,1,1,3,3,3,1,1,0},
+				{0,1,3,3,3,3,1,1,0,1,1,3,3,3,3,1},
+				{1,3,3,3,3,1,0,0,0,0,0,1,3,3,3,1},
+				{0,1,3,3,1,0,0,0,0,0,0,0,1,3,1,0},
+				{0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0},
 				};
 		
-		int po2x = 720;
-		int po2y = 220;
+		int po2x = 470;
+		int po2y = 290;
 		
 		for(int x = 0;x<16;x++)
 		{
-			for(int y = 0;y<15;y++)
+			for(int y = 0;y<22;y++)
 			{
 				switch(planta[y][x])
 				{
@@ -360,6 +366,13 @@ public class Ventana extends JFrame implements MouseListener{
 				
 			}
 		}
+		g2d.setColor(Color.red);
+		g2d.setFont(new Font(Font.SANS_SERIF,Font.BOLD,30));
+		g2d.drawString("Mario", 70, 70);
+		g2d.setColor(Color.white);
+		
+		g2d.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
+		g2d.drawString("  X   5", 80, 90);
 	}
 	
 
